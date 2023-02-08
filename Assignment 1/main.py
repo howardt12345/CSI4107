6aab405cbd749c8f56af3da49fa7f4fbd5e54871
Howard Tseng <howardt12345@gmail.com>
2023-02-08 13:24:48 -0500

    feat: preprocessing

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -1,3 +1,19 @@
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
 import pyterrier as pt
+import string
+import re
+import os
+import nltk
+nltk.download('punkt')
+from preprocessing import preprocess_directory, preprocess
+
+
 if not pt.started():
   pt.init()
+
+# Preprocess the collection
+preprocessed_documents = preprocess_directory('AP_collection/coll')
+
+# show the first document
+print(preprocessed_documents[0])
---
+++ Assignment 1/preprocessing.py
@@ -0,0 +1,79 @@
+# Functions and classes for preprocessing the data
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+import string
+import re
+import os
+import nltk
+nltk.download('punkt')
+
+class Document:
+  def __init__(self, doc_no, doc_text, tokens):
+    self.doc_no = doc_no
+    self.doc_text = doc_text
+    self.tokens = tokens
+
+  def __str__(self):
+    return 'Document Number: ' + self.doc_no + '\nDocument Text: ' + self.doc_text + '\nTokens: ' + str(self.tokens) + '\n'
+
+
+# Get the stop words
+def get_stop_words():
+  stopwords = set()
+  # Open the stop words and add them to the set
+  with open('StopWords.txt', 'r') as file:
+    for line in file:
+      stopwords.add(line.strip())
+  return stopwords
+
+# initialize the stemmer
+stemmer = PorterStemmer()
+
+# load the stopwords
+stop_words = get_stop_words()
+
+# function to perform preprocessing on the text
+def preprocess(file):
+  with open(file, "r") as f:
+    content = f.read()
+  documents = re.findall(r'<DOC>(.*?)</DOC>', content, re.DOTALL)
+  preprocessed_documents = []
+  for document in documents:
+    # Get the document number and text
+    raw_no = re.search(r'<DOCNO>(.*?)</DOCNO>', document, re.DOTALL)
+    doc_no = raw_no.group(1) if raw_no else ''
+    raw_text = re.search(r'<TEXT>(.*?)</TEXT>', document, re.DOTALL)
+    doc_text = raw_text.group(1) if raw_text else ''
+
+    # lowercase the text
+    doc_text = doc_text.lower()
+
+    # tokenize the text
+    tokens = word_tokenize(doc_text)
+    #lowercase all tokens
+    tokens = [token.lower() for token in tokens]
+    #remove stopwords
+    tokens = [token for token in tokens if token not in stop_words]
+    #apply the porter stemmer
+    stemmer = PorterStemmer()
+    stemmed_tokens = [stemmer.stem(token) for token in tokens]
+    #remove punctuation
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in stemmed_tokens]
+
+    # remove empty tokens
+    stripped = [token for token in stripped if token]
+
+    # create a document object
+    doc = Document(doc_no, doc_text, stripped)
+    preprocessed_documents.append(doc)
+  return preprocessed_documents
+
+# main function to preprocess a directory of text files
+def preprocess_directory(directory):
+  preprocessed_documents = []
+  for filename in os.listdir(directory):
+    print('Processing file: ', filename)
+    file = os.path.join(directory, filename)
+    preprocessed_documents.extend(preprocess(file))
+  return preprocessed_documents
