a3dcaae2391d3b396f801b0cc9f51e48f63f89f1
Howard Tseng <howardt12345@gmail.com>
2023-02-08 17:17:24 -0500

    feat: index in memory

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -16,15 +16,14 @@ preprocessed_documents = preprocess_directory('AP_collection/coll', 1)
 df = pd.DataFrame.from_records([doc.to_dict() for doc in preprocessed_documents])
 df.head()
 
-print(df)
-
 # Create a Terrier index from the dataframe
-pd_indexer = pt.DFIndexer("pd_index")
-indexref = pd_indexer.index(df["tokens"], df["docno"])
+pd_indexer = pt.DFIndexer("./pd_index", type=pt.IndexingType.MEMORY)
+indexref = pd_indexer.index(df["doctext"], df["docno"])
 
 # Create a BM25 retrieval model
 bm25 = pt.BatchRetrieve(indexref, wmodel="BM25")
 
 # use the BM25 model to retrieve the top 10 documents for the query "information retrieval"
-bm25.search("information retrieval", wmodel="BM25", k=10)
+result = bm25.search("vietnam")
+print(result)
 
