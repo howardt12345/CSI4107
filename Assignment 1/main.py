a1e03ab6005a4affdaf4b8d3951b6960c23238f3
Howard Tseng <howardt12345@gmail.com>
2023-02-08 19:24:45 -0500

    feat: generating index if it doesn't exist

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -10,17 +10,25 @@ from preprocessing import preprocess_directory, preprocess
 if not pt.started():
   pt.init()
 
-# Preprocess the collection
-# preprocessed_documents = preprocess_directory('AP_collection/coll')
+def generate_index():
+  # Preprocess the collection
+  preprocessed_documents = preprocess_directory('AP_collection/coll')
 
-# Create a dataframe from the preprocessed documents
-# df = pd.DataFrame.from_records([doc.to_dict() for doc in preprocessed_documents])
-# df.head()
+  # Create a dataframe from the preprocessed documents
+  df = pd.DataFrame.from_records([doc.to_dict() for doc in preprocessed_documents])
+  df.head()
 
-# Create a Terrier index from the dataframe
-# pd_indexer = pt.IterDictIndexer(os.path.abspath('./pd_index'))
-# indexref = pd_indexer.index(df.to_dict(orient='records'))
-indexref = pt.IndexFactory.of(os.path.abspath('./pd_index/data.properties'))
+  # Create a Terrier index from the dataframe
+  pd_indexer = pt.IterDictIndexer(os.path.abspath('./pd_index'), overwrite=True)
+  indexref = pd_indexer.index(df.to_dict(orient='records'))
+
+  return indexref
+
+# Check if the index exists, if not create it
+if not os.path.exists('./pd_index'):
+  indexref = generate_index()
+else:
+  indexref = pt.IndexFactory.of(os.path.abspath('./pd_index/data.properties'))
 
 # Create a BM25 retrieval model
 bm25 = pt.BatchRetrieve(indexref, wmodel="BM25")
