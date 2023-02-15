e02dc58474cf3c77369f8d9d66d9b0f941cea2b7
Howard Tseng <howardt12345@gmail.com>
2023-02-14 20:07:47 -0500

    Revert "cleanup: removed testing files and code"
    This reverts commit 0afe758a28e75b9efc560f768fc0425f18263ea0.

--- Assignment 1/main.py
+++ Assignment 1/main.py
@@ -35,6 +35,37 @@ else:
 
 # Create a retrieval model
 model = pt.BatchRetrieve(indexref, wmodel="TF_IDF", num_results=1000)
-
-# Query the model and write the results
 query_retrieve(model)
+
+# # Query and write to
+# topics = extract_topics("topics1-50.txt")
+
+# bm_file_out = open('Results.txt', 'w')
+
+# for i in range(len(topics)):
+#     print("topic " + str(i) +": " + topics[i])
+#     curr_result = bm25.search(preprocess_text((str(topics[i]))))
+#     for j in range(len(curr_result)):
+#         result_row = curr_result.iloc[j]
+#         bm_file_out.write(str(i+1) + " " + "Q0 " + result_row['docno'] + " " + str(result_row['rank']+1) + " " + str(result_row['score']) + " " + "runid\n")
+        
+# bm_file_out.close()
+
+
+
+# # use the BM25 model to index
+# result = bm25.search("Coping with overcrowded prisons")
+# print('BM25')
+# print(result)
+
+# #print file out to Results 
+# bm_file_out = open('Results.txt', 'w')
+# bm_file_out.write(result.to_string())
+# bm_file_out.close()
+
+# # Use the tf-idf retrieval model to index
+# tfidf = pt.BatchRetrieve(indexref, wmodel="TF_IDF")
+# result = tfidf.search("Coping with overcrowded prisons")
+# print('\nTF-IDF')
+# print(result)
+
