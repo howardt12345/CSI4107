7a09d7d28080d6cb44e1645a90e5976c61c13b52
Howard Tseng <howardt12345@gmail.com>
2023-02-14 21:53:24 -0500

    feat: query func

--- Assignment 1/retrieval.py
+++ Assignment 1/retrieval.py
@@ -26,12 +26,7 @@ def query_retrieve(model, descriptions=False, runid='runid', filename='Results.t
   bm_file_out = open(filename, 'w')
 
   for i, topic in enumerate(topics):
-    t = " ".join(preprocess_text(topic['title'], stem=False, stopwords=False))
-    if descriptions:
-      t += " " + \
-          " ".join(preprocess_text(
-              topic['description'], stem=False, stopwords=False))
-    curr_result = model.search(t)
+    curr_result = query(model, topic, descriptions)
     for j in range(len(curr_result)):
       result_row = curr_result.iloc[j]
       bm_file_out.write(str(i+1) + " " + "Q0 " + result_row['docno'] + " " + str(
@@ -39,3 +34,11 @@ def query_retrieve(model, descriptions=False, runid='runid', filename='Results.t
 
   bm_file_out.close()
   print("Written results to file: " + filename)
+
+def query(model, topic, descriptions=False):
+  t = " ".join(preprocess_text(topic['title'], stem=False, stopwords=False))
+  if descriptions:
+    t += " " + \
+        " ".join(preprocess_text(
+            topic['description'], stem=False, stopwords=False))
+  return model.search(t)
